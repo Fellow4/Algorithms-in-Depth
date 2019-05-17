c91cd23de46c2cd04391e70870e761008a03da2d
Fellow4 <43861622+Fellow4@users.noreply.github.com>
2019-05-18 06:43:18 +1200

    Add files via upload
    Solution deals with non dynamic inputs

---
+++ Assignment 1/Solution.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    float data;
+    struct node* ptr;
+}node;
+
+ void print(struct node* root){
+    while(1){
+        printf("%f", root->data);
+        printf(" ");
+        if(root->ptr==NULL) break;
+        root=root->ptr;
+    }
+    printf("\n");
+    return ;
+ }
+
+void split(node *a,node **l,node **r){
+   int counter=0;
+   node *t=a;
+   while(t != NULL) {
+        t=t->ptr;
+        counter++;
+   }
+   int val=0;
+   t=a;
+   while(1){
+    val++;
+    if(val == counter/2) break;
+    t=t->ptr;
+   }
+   *r=t->ptr;
+   t->ptr=NULL;
+   *l=a;
+   return ;
+}
+
+node* mergelist(node *a,node *b){
+   node *p=a,*q=b,*temp,*counter;
+   if(p->data < q->data){
+        temp=p;
+        counter=p;
+        p=p->ptr;
+   }
+   else {
+        temp=q;
+        counter=q;
+        q=q->ptr;
+   }
+   while(p!=NULL && q!=NULL){
+     if(p->data < q->data){
+        temp->ptr=p;
+        temp=p;
+        p=p->ptr;
+     }
+     else {
+        temp->ptr=q;
+        temp=q;
+        q=q->ptr;
+     }
+   }
+   if(q == NULL) temp->ptr=p;
+   else if(p == NULL) temp->ptr=q;
+   return counter;
+}
+
+
+
+void mergesort(node **ref){
+   node *head = *ref;
+   node *a;
+   node *b;
+   if(head->ptr == NULL) return;
+   split(head,&a,&b);
+   mergesort(&a);
+   mergesort(&b);
+   *ref=mergelist(a,b);
+}
+
+
+void push(node **ref,float val){
+    node *temp=(node*)malloc(sizeof(node));
+    temp->data = val;
+    temp->ptr=*ref;
+    *ref=temp;
+}
+#define n 1e3
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    node **org=(node**)malloc((n+1)*sizeof(node*));
+    for(int i=0;i<n+1;++i) org[i]=NULL;
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;++i){
+    int x,y,z,u,v;
+    float val;
+    scanf("%d %d:%d-%d:%d %f",&x,&y,&z,&u,&v,&val)
+    }
+    }
+    for(int i=1;i<n+1;++i) mergesort(&org[i]);
+    for(int i=1;i<n+1;++i){
+        printf("%d ",i);
+        print(org[i]);
+    }
+    return 0;
+}
